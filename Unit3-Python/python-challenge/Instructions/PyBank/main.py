ca0fe3d92042e76ce642dccd35a1a82b16bc3175
Dave Eum <eumjungsik@gmail.com>
2019-10-14 19:54:13 -0400

    more code changes to PyBank
    added calculations to find calculation between months.

--- Unit3-Python/python-challenge/Instructions/PyBank/main.py
+++ Unit3-Python/python-challenge/Instructions/PyBank/main.py
@@ -11,7 +11,7 @@ date_list = []
 total_month = 0
 total_revenue = 0
 total_change = 0
-profit = 0
+initial_profit = 0
 
 
 #Open csv
@@ -21,10 +21,24 @@ with open(bank_csv, newline="",) as csvfile:
     csv_header = next(csvreader)
     for row in csvreader:
         total_month = total_month + 1
+        date_list.append(row[0])
+        profit_list.append(row[1])
+        total_revenue = total_revenue + int(row[1])
+        final_profit = int(row[1])
+        monthly_change = final_profit - initial_profit
+        monthly_change_list.append(monthly_change)
+        total_change = total_change + monthly_change
+        initial_profit = final_profit
         
 
-
+        
+print(int(initial_profit))
+print(str(monthly_change))
+print(int(final_profit))
+print(int(total_revenue))
 print(str(total_month))
+print(str(profit_list))
+#print(str(date_list))
         #list(csvreader)
         #print(csvreader)
 #month.append(row[0])

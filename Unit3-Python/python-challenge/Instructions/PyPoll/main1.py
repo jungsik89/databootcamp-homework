cfc37a510ff0b5afb806dee990a2083290418c29
Dave Eum <eumjungsik@gmail.com>
2019-10-14 11:10:04 -0400

    fixed and added some new code
    csvreader

--- Unit3-Python/python-challenge/Instructions/PyPoll/main1.py
+++ Unit3-Python/python-challenge/Instructions/PyPoll/main1.py
@@ -1,23 +1,40 @@
 import os, csv
 
-election_csv = os.path.join('..','Resources','election_data.csv')
+election_csv = os.path.join('Resources','election_data.csv')
 
-khanvotecount = ['votecount': 0, 'win percentage': 0.0, 'totalwin': 0, )
-correyvotecount = ('votecount': 0, 'win percentage': 0.0, 'totalwin': 0, )
-livotecount = ('votecount': 0, 'win percentage': 0.0, 'totalwin': 0, )
+khanvotecount = {'votecount': 0, 'win percentage': 0.0, 'totalwin': 0}
+correyvotecount = {'votecount': 0, 'win percentage': 0.0, 'totalwin': 0} 
+livotecount = {'votecount': 0, 'win percentage': 0.0, 'totalwin': 0}
 
-with open(election_csv, 'w') as csvreader:
-    reader = csv.reader(csvreader, delimiter = ",")
-    data = list(reader)
-    row_count = len(data)
 
-def percentage(election_data):
 
-    candidatename = str(election_data[2])
-    voterid = str(election_data[0])
-    county = str(election_data[1])
+#def khanpercentage(election_data):
 
-print(f"this is {Candidate}")
+with open(election_csv, 'r', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter = ',')   
+    for row in reader:
+        if row[1] == 'Khan':
+            khanvotecount.append[row[1]]
+            print('khanvotecount')
+
+        
+            # print(reader)
+        #row_count = len(reader)
+        #print(row_count)
+
+
+        #data = list(reader)
+        #row_count = len(data)
+        #print(row_count)
+  #      candidatename = str(election_data[2])
+   #     voterid = str(election_data[0])
+    #    county = str(election_data[1])
+
+   # for row in range(row_count):
+    #    If 
+
+
+#print(f"this is {Candidate}")
 
 
     
